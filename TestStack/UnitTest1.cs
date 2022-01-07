65074d1ca00ac7aea42692d9ac80debc49031253
Devilboi99 <Turnickii.n@gmail.com>
2022-01-07 10:14:33 +0500

    First commit

---
+++ TestStack/UnitTest1.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using System;
+using steak;
+
+namespace TestStack
+{
+    [TestFixture]
+    public class TestCasePush
+    {
+
+        [TestCase(10)]
+        public void Test(int count)
+        {
+            var stack = new Stack<int>();
+            for (var j = 0; j < count; j++)
+            {
+                stack.Push(j);
+            }
+        }
+
+        [TestCase(24, 21)]
+        public void TestPop(int countPush, int countPop)
+        {
+            
+
+        }
+
+        [TestFixture]
+    public class TestStackPop
+    {
+    }
+
+}
+}
---
+++ testing my steak/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using steak;
+
+namespace testing_my_steak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = new Stack<int>();
+            name.Push(32); ;
+            Console.WriteLine(name.Top());
+            name.Pop();
+            Console.WriteLine(name.Top());
+            name.Push(14);
+            name.Push(12);
+            Console.WriteLine(name.Pop());
+            Console.WriteLine(name.Size);
+        }
+    }
+}
